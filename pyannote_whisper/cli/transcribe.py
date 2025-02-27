0ecd52030bd427c89208af47f122f978bf9fd248
Paul Creaser <42903437+PaulCreaserML@users.noreply.github.com>
2025-02-27 09:45:53 +0900

    Update transcribe.py
    Update speaker-diarization-3.1

--- pyannote_whisper/cli/transcribe.py
+++ pyannote_whisper/cli/transcribe.py
@@ -98,7 +98,7 @@ def cli():
     diarization = args.pop("diarization")
     if diarization:
         from pyannote.audio import Pipeline
-        pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization",
+        pipeline = Pipeline.from_pretrained("speaker-diarization-3.1",
                                             use_auth_token="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")
 
     for audio_path in args.pop("audio"):
